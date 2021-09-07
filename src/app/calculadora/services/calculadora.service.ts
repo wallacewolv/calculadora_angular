7fcb67bf92f9da9a5c4777b6cede93aaab483b09
Wallace <wallace_wolv@hotmail.com>
2021-09-06 21:09:31 -0300

    Adicionado Service, Testes e Lógica

--- src/app/calculadora/services/calculadora.service.ts
+++ src/app/calculadora/services/calculadora.service.ts
@@ -1,3 +1,11 @@
+/**
+* Serviço responsável por executar as operações
+* da calculadora
+*
+*@author Wallace W. <wallace_wolv@hotmail.com>
+*@since 1.0.0
+*/
+
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +13,48 @@ import { Injectable } from '@angular/core';
 })
 export class CalculadoraService {
 
+  /**
+  * Define as constantes utilizadas para identificar
+  * as operações de cálculo
+  */
+  static readonly SOMA: string = '+';
+  static readonly SUBTRACAO: string = '-';
+  static readonly DIVISAO: string = '/';
+  static readonly MULTIPLICACAO: string = '*';
+
   constructor() { }
+
+  /**
+   * Método que calcula uma operação matemática dado
+   * dois númeors.
+   * Suporta as operações soma, subtração, divisão
+   * e multiplicação.
+   *
+   * @param num1 number
+   * @param num2 number
+   * @param operacao string Operacao a ser executada
+   * @return number Resultado da operação
+   */
+  calcular(num1: number, num2: number, operacao: string): number {
+    let resultado: number; // armazena o resultado da operação
+
+    switch (operacao) {
+      case CalculadoraService.SOMA:
+        resultado = num1 + num2;
+        break;
+      case CalculadoraService.SUBTRACAO:
+        resultado = num1 - num2;
+        break;
+      case CalculadoraService.DIVISAO:
+        resultado = num1 / num2;
+        break;
+      case CalculadoraService.MULTIPLICACAO:
+        resultado = num1 * num2;
+        break;
+      default:
+        resultado = 0;
+    }
+
+    return resultado;
+  }
 }
